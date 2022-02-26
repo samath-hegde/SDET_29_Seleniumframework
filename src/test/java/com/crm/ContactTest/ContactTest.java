0f402af06c4221b20557491874d36befe5b3e549
samath-hegde <samathhegde11@gmail.com>
2022-02-26 23:22:26 +0530

    my first commit

---
+++ src/test/java/com/crm/ContactTest/ContactTest.java
@@ -0,0 +1,15 @@
+package com.crm.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	 public void createContactTest() {
+		System.out.println("execute create contact test");
+	}
+	@Test
+	 public void modifyContactTest() {
+		System.out.println("execute modify contact test");
+}
+}
---
+++ src/test/java/com/crm/OrgTest/OrganizationTest.java
@@ -0,0 +1,19 @@
+package com.crm.OrgTest;
+
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	
+	@Test
+	
+     public void createorgTest() {
+		 System.out.println("execute createorg test");
+		 }
+    @Test
+    
+    public void modifyorgTest() {
+    	
+    	System.out.println("execute modify org test");
+}
+}
